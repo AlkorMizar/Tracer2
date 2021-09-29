8377634f9c4779cd99fa40a673cf20899a878933
AlkorMizar <anisa15012002@gmail.com>
2021-09-29 12:30:57 +0300

    ver 1.2.1

--- Program.cs
+++ Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading;
 using Tracer2.TracerAPI;
 using Tracer2.TracerAPI.Serializer;
 using Tracer2.TracerAPI.Writer;
