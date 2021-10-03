e773faebf1e0a3ee8ebae9e29c7bfa36db8a77e1
AlkorMizar <anisa15012002@gmail.com>
2021-10-03 15:53:42 +0300

    ver 1.8 final

--- Program.cs
+++ Program.cs
@@ -16,46 +16,14 @@ namespace Tracer2
 
             _tracer = new Tracer();
             _tracer.StartTrace();
-            M0();
-            Thread th = new Thread(M0);
-            th.Start();
-            Thread th2 = new Thread(M1);
-            th2.Start();
-            th.Join();
-            th2.Join();
+            B clB = new B(_tracer);
+            clB.MethodB1();
+            clB.MethodB2();
             _tracer.StopTrace();
             IWriter wr = new ConsoleWriter();
             ISerializer ser = SerializerJSON_NET.GetInstance();
             wr.Write(ser.Serialize(_tracer.GetTraceResult()));
         }
 
-        public static void M0()
-        {
-            _tracer.StartTrace();
-            M1();
-            M2();
-            M2();
-            _tracer.StopTrace();
-        }
-
-        private static void M1()
-        {         
-            _tracer.StartTrace();
-            M2();
-            M2();
-            M2();
-            int i = int.MaxValue/100;
-            while (i-- > 0) ;
-            _tracer.StopTrace();
-            
-        }
-
-        private static void M2()
-        {
-            _tracer.StartTrace();
-            int i = int.MaxValue / 100;
-            while (i-- > 0) ;
-            _tracer.StopTrace();
-        }
     }
 }
---
+++ Test.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Tracer2.TracerAPI;
+
+namespace Tracer2
+{
+    class Sleep {
+        public static void sleep() {
+            int i = int.MaxValue/40;
+            while (i-- > 0);
+        }
+    }
+
+    class A {
+        ITracer tracer;
+        public A(ITracer _tracer)
+        {
+            tracer = _tracer;
+        }
+
+        public void MethodA1() {
+            tracer.StartTrace();
+            Sleep.sleep();
+            MethodA2();
+            MethodA3();
+            tracer.StopTrace();
+        }
+        public void MethodA2()
+        {
+            tracer.StartTrace();
+            Sleep.sleep();
+            MethodA3();
+            tracer.StopTrace();
+        }
+        public void MethodA3()
+        {
+            tracer.StartTrace();
+            Sleep.sleep();
+            tracer.StopTrace();
+        }
+    }
+
+    class B {
+
+        ITracer tracer;
+        A clA;
+        C clC;
+        public B(ITracer _tracer)
+        {
+            tracer = _tracer;
+            clA = new A(tracer);
+            clC = new C(tracer);
+        }
+        public void MethodB1()
+        {
+            tracer.StartTrace();
+            Sleep.sleep();
+            Thread thA = new Thread(clA.MethodA1);
+            Thread thC = new Thread(clC.MethodС1);
+            thA.Start();
+            thC.Start();
+            MethodB2();
+            thA.Join();
+            thC.Join();
+            tracer.StopTrace();
+        }
+
+        public void MethodB2()
+        {
+            tracer.StartTrace();
+            Sleep.sleep();
+            tracer.StopTrace();
+        }
+    }
+
+    class C {
+        ITracer tracer;
+        public C(ITracer _tracer)
+        {
+            tracer = _tracer;
+        }
+        public void MethodС1()
+        {
+            tracer.StartTrace();
+            Sleep.sleep();
+            MethodС2();
+            MethodС3();
+            MethodС3();
+            MethodС2();
+            MethodС3();
+            tracer.StopTrace();
+        }
+        public void MethodС2()
+        {
+            tracer.StartTrace();
+            Sleep.sleep();
+            MethodС3();
+            MethodС3();
+            tracer.StopTrace();
+        }
+        public void MethodС3()
+        {
+            tracer.StartTrace();
+            Sleep.sleep();
+            tracer.StopTrace();
+        }
+    }
+}
